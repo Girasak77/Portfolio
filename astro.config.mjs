df3d5085f310b6013224d734388a7d10ad44f7a2
Girasak77 <girasnext@gmail.com>
2026-06-23 17:09:19 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // ตั้งค่า Astro ให้ใช้ Tailwind ผ่าน Vite
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+export default defineConfig({
+  site: 'https://girasak77.github.io',
+  base: '/Portfolio/',
+  vite: { plugins: [tailwindcss()] },
+});
